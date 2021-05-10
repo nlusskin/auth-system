220811991b662796b338ae75ae1adbfd8e45410e
Nicholas <lusskin11@gmail.com>
2021-05-10 12:28:29 -0400

    init

---
+++ app.ts
@@ -0,0 +1,43 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import createError from 'http-errors';
+import logger from 'morgan';
+import path from 'path';
+
+import indexRouter from './routes/index';
+import authRouter from './routes/auth';
+
+const PORT = process.env.port || 3033;
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/authenticate', authRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+app.listen(PORT, () => console.info('Server listening on port: ' + PORT))
